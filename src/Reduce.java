cb43982781f4904613179f40baeac30b4c24bcb1
Alicia <Alicia@az-macbook-air.local>
2023-09-11 10:31:18 -0400

    created Reduce file

--- src/Reduce.java
+++ src/Reduce.java
@@ -1,2 +1,18 @@
 public class Reduce {
+    public static void main(String[] args) {
+        int n = 100;
+        int steps = 0;
+        while (n != 0) {
+            boolean even = n % 2 == 0;
+
+            if (even) {
+                n = n / 2;
+            }
+            else {
+                n = n - 1;
+            }
+            steps++;
+        }
+        System.out.println(steps);
+    }
 }
